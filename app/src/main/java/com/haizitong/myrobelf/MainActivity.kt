1d02716fe93bd25f9478b719cf4e2e2d5f40ba96
shiwenbin <shiwenbin@haizitong.com>
2019-05-29 17:27:22 +0800

    提交测试

--- app/src/main/java/com/haizitong/myrobelf/MainActivity.kt
+++ app/src/main/java/com/haizitong/myrobelf/MainActivity.kt
@@ -2,11 +2,13 @@ package com.haizitong.myrobelf
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("tt", "test")
     }
 }
